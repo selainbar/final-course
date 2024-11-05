966bf54bf60717f857104ed84b6777b946cc1c61
sela inbar <selainbar14@gmail.com>
2024-11-05 16:01:07 +0200

    final stage(game added)

--- backend/game/game.js
+++
@@ -1,4 +0,0 @@
-// find the game online and how to display it 
-// build a socket to host the games or use the chat socket to create new room for each game=> figure out which one is better or even possible
-//connect to mongoDB to record the game history with the model of 
-//{white player,black player,time of start,duration of the game ,time of end , who won,by how much,}
